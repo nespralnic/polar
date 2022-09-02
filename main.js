8e47f6dfff276995eab8ff3a3dc616ab2b6c3581
nespralnic <niconespral@gmail.com>
2022-09-02 09:47:08 -0300

    fotos

--- main.js
+++ main.js
@@ -1,17 +1,142 @@
+// hardcode pictures
+let picsBase = [
+    {
+    "id": 1,
+    "url": "./images/pelis/afterthe.jpeg",
+    "categories": []
+    },
+    {
+    "id": 2,
+    "url": "./images/pelis/reygitano.jpg",
+    "categories": []
+    },
+        {
+    "id": 3,
+    "url": "./images/pelis/biutiful.jpg",
+    "categories": []
+    },
+    {
+    "id": 4,
+    "url": "./images/pelis/blackhollow.jpg",
+    "categories": []
+    },   
+    {
+    "id": 5,
+    "url": "./images/pelis/bloodbonding.jpg",
+    "categories": []
+    },
+        {
+    "id": 6,
+    "url": "./images/pelis/contetrencat.jpeg",
+    "categories": []
+    },
+    {
+    "id": 7,
+    "url": "./images/pelis/cruzdelsur.jpeg",
+    "categories": []
+    },
+    {
+    "id": 8,
+    "url": "./images/pelis/diezmil.jpg",
+    "categories": []
+    },
+    {
+    "id": 9,
+    "url": "./images/pelis/elperdido.jpg",
+    "categories": []
+    },
+        {
+    "id": 10,
+    "url": "./images/pelis/exile.jpg",
+    "categories": []
+    },
+    {
+    "id": 11,
+    "url": "./images/pelis/lagallina.jpg",
+    "categories": []
+    },   
+    {
+    "id": 12,
+    "url": "./images/pelis/luz.jpg",
+    "categories": []
+    },
+        {
+    "id": 13,
+    "url": "./images/pelis/maniac.jpeg",
+    "categories": []
+    },
+    {
+    "id": 14,
+    "url": "./images/pelis/monstresdepaper.jpg",
+    "categories": []
+    },
+    {
+    "id": 15,
+    "url": "./images/pelis/nana.jpeg",
+    "categories": []
+    },   
+    {
+    "id": 16,
+    "url": "./images/pelis/plenamar.jpg",
+    "categories": []
+    },
+        {
+    "id": 17,
+    "url": "./images/pelis/prescindibles.jpg",
+    "categories": []
+    },
+    {
+    "id": 18,
+    "url": "./images/pelis/recursoshumanos.jpg",
+    "categories": []
+    },
+    {
+    "id": 16,
+    "url": "./images/pelis/red dress.jpg",
+    "categories": []
+    },
+    {
+    "id": 1,
+    "url": "./images/pelis/todoparecia.jpg",
+    "categories": []
+    },
+]
+
+
 //DISPLAY DE CARDS
 
 let cardContainer = document.querySelector(".card-container")
-let cont = 1;
 let scrollMax=0;
 let scrolling = false
 let scrollDown = false
 let lengthArray = 10
 //let contadorIntervalo = 0
 
-console.log(window.innerWidth);
+
+//fetch pictures
+console.log(picsBase);
+let arrayPic = []
+picsBase.forEach(element => {
+    arrayPic.push(element.url);
+});
+
+console.log(arrayPic)
+
+// starting foto delivery
+//let arrayPic = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+let counterPic = 0;
+let totalPic = arrayPic.length
+
+function deliverPic(arrayPic){
+    let x = arrayPic[counterPic]
+    counterPic+=1
+    return `<img src="${x}" alt="" class="card-image"></img>`
+}
+
+
+
 
 let setDisplay = 0
-
 function displayStartF(){
     let initialWidth = window.innerWidth
 
@@ -36,13 +161,12 @@ function displayStartF(){
             break;
     }
 
-    console.log(setDisplay);
-
     
     for (let index = 0; index < setDisplay*2; index++) {
         setTimeout(()=>{
             let contenido = document.createElement("div")
             contenido.classList.add("card","new-card")
+            contenido.innerHTML = deliverPic(arrayPic)
             cardContainer.appendChild(contenido);
         },index*200)
         
@@ -52,6 +176,10 @@ function displayStartF(){
 //MAIN
 //print 2 complete rows of initial cards depending on viewport size
 displayStartF()
+
+
+
+
 
 //"light" event
 window.addEventListener('scroll',()=>{
@@ -69,34 +197,25 @@ const interv = setInterval(() => {
         scrollDown = false
     }
 
-    //contadorIntervalo++
-
-    //console.log("ContInt" + contadorIntervalo + "scroll ON: "+scrolling + "scroll DOWN: "+scrollDown);
+    
     if (scrolling && scrollDown) {
         scrolling = false;
         //setDisplay completes each row
         for (let index = 0; index < setDisplay; index++) {
             setTimeout( ()=>{
-            
-                cont +=1
-                let contenido = document.createElement("div")
-                contenido.classList.add("card","new-card")
-                contenido.innerHTML = cont
-                
-                //fin del intervalo con el length de fotos
-                if (cont < lengthArray ){
+                if (counterPic < totalPic ){
+                    let contenido = document.createElement("div")
+                    contenido.classList.add("card","new-card")
+                    contenido.innerHTML = deliverPic(arrayPic)
                     cardContainer.appendChild(contenido);
-                    //console.log("insert " + cont + "Ciclo: " + index);
-                    //console.log("Window: " + window.scrollY + "Max: "+ scrollMax)
-                    
-                } else {
-                        clearInterval(interv)
+                    console.log(counterPic +" "+ totalPic);
+                }else{
+                    clearInterval(interv)
                 }
-            },index*300)    
-        }
-    }
-    
-    
+                },index*300
+            )   
+        }    
+    } 
 },1200);
 
 
